222665752a9e041d2967e5c5ab9c8c775abe09cc
Pigpog <7060603@hotmail.ca>
2019-06-26 01:08:24 -0400

    Removed unneeded stuff

--- keyimgextract.c
+++ keyimgextract.c
@@ -4,10 +4,8 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <assert.h>
 #include <unistd.h>
-#include <stdbool.h>
 
 #include "toxkeyimg.h"
 
@@ -17,13 +15,9 @@ int main(int argc, char *argv[])
         printf("Usage: [Path]\n");
         return -1;
     }
-    const char *path = argv[1];
 
-    /* Validate generated image */
     char key_out[BUF_SIZE] = {'\0'};
-
-    assert (extract_keyimg(key_out, path) == 0);
-
+    assert (extract_keyimg(key_out, argv[1]) == 0);
     printf("KEY:%s\n\n", key_out);
     return 0;
 }
